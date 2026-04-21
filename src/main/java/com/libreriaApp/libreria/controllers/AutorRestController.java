3129acd7e208d7adae3285bec95d75a73a4e4002
Walter Gamarra <waltergamarra572@gmail.com>
2026-04-21 19:52:54 -0300

    Fix - @RequestMapping de controllers

--- src/main/java/com/libreriaApp/libreria/controllers/AutorRestController.java
+++ src/main/java/com/libreriaApp/libreria/controllers/AutorRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/autor")
+@RequestMapping("/api/v1/tienda/admin/autor")
 public class AutorRestController {
 
     private IAutorService autorService;
